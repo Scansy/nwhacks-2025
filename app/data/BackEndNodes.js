a49795550fee97586fe80ef5b4dbc342ca9c4ba7
Diaggio <123846531+Diaggio@users.noreply.github.com>
2025-01-19 07:03:27 -0800

    changed data structure

---
+++ app/data/BackEndNodes.js
@@ -0,0 +1,232 @@
+const nodes=[
+    {
+    "ID": 300,
+    "NodeName": "Internet of Things",
+    "Description": "Network of physical objects embedded with sensors, software, and other technologies for exchanging data with other devices over the internet.",
+    "x": 946,
+    "y": 57,
+    "group": "backend"
+  },
+  {
+    "ID": 301,
+    "NodeName": "HTTP",
+    "Description": "Hypertext Transfer Protocol, the foundation of data communication for the World Wide Web.",
+    "x": 700,
+    "y": 52,
+    "group": "backend"
+  },
+  {
+    "ID": 302,
+    "NodeName": "DNS",
+    "Description": "Domain Name System that translates human-readable domain names to IP addresses.",
+    "x": 690,
+    "y": 306,
+    "group": "backend"
+  },
+  {
+    "ID": 303,
+    "NodeName": "Client-Server",
+    "Description": "Computing model where client devices request and receive services/resources from centralized servers.",
+    "x": 626,
+    "y": 123,
+    "group": "backend"
+  },
+  {
+    "ID": 304,
+    "NodeName": "Browsers",
+    "Description": "Software applications for accessing, retrieving, and navigating information on the World Wide Web.",
+    "x": 460,
+    "y": 265,
+    "group": "backend"
+  },
+  {
+    "ID": 305,
+    "NodeName": "Hosting",
+    "Description": "Service that allows websites and web applications to be accessible via the World Wide Web.",
+    "x": 531,
+    "y": 195,
+    "group": "backend"
+  },
+  {
+    "ID": 306,
+    "NodeName": "Back End Languages",
+    "Description": "Programming languages specifically used for server-side development and business logic.",
+    "x": 409,
+    "y": 387,
+    "group": "backend"
+  },
+  {
+    "ID": 307,
+    "NodeName": "JavaScript",
+    "Description": "High-level programming language primarily used for creating interactive effects within web browsers.",
+    "x": 515,
+    "y": 435,
+    "group": "backend"
+  },
+  {
+    "ID": 308,
+    "NodeName": "Python",
+    "Description": "High-level programming language known for its simplicity and wide range of applications including web development and data science.",
+    "x": 554,
+    "y": 290,
+    "group": "backend"
+  },
+  {
+    "ID": 309,
+    "NodeName": "Ruby",
+    "Description": "Dynamic, object-oriented programming language designed for simplicity and productivity.",
+    "x": 377,
+    "y": 504,
+    "group": "backend"
+  },
+  {
+    "ID": 310,
+    "NodeName": "Go",
+    "Description": "Statically typed programming language developed by Google, known for its efficiency and concurrent programming support.",
+    "x": 530,
+    "y": 347,
+    "group": "backend"
+  },
+  {
+    "ID": 311,
+    "NodeName": "Version Control",
+    "Description": "System for tracking and managing changes to software code and documentation.",
+    "x": 531,
+    "y": 514,
+    "group": "backend"
+  },
+  {
+    "ID": 312,
+    "NodeName": "Git",
+    "Description": "Distributed version control system for tracking changes in source code during software development.",
+    "x": 1072,
+    "y": 362,
+    "group": "backend"
+  },
+  {
+    "ID": 313,
+    "NodeName": "GitHub",
+    "Description": "Web-based hosting service for version control using Git and collaboration platform for developers.",
+    "x": 1005,
+    "y": 429,
+    "group": "backend"
+  },
+  {
+    "ID": 314,
+    "NodeName": "Database",
+    "Description": "Organized collection of structured information or data typically stored electronically.",
+    "x": 905,
+    "y": 468,
+    "group": "backend"
+  },
+  {
+    "ID": 315,
+    "NodeName": "MySQL",
+    "Description": "Open-source relational database management system using structured query language.",
+    "x": 780,
+    "y": 524,
+    "group": "backend"
+  },
+  {
+    "ID": 316,
+    "NodeName": "Oracle",
+    "Description": "Commercial relational database management system known for enterprise-level applications.",
+    "x": 619,
+    "y": 459,
+    "group": "backend"
+  },
+  {
+    "ID": 317,
+    "NodeName": "APIs",
+    "Description": "Application Programming Interfaces that define interactions between multiple software applications.",
+    "x": 651,
+    "y": 429,
+    "group": "backend"
+  },
+  {
+    "ID": 318,
+    "NodeName": "REST",
+    "Description": "Representational State Transfer, architectural style for distributed hypermedia systems.",
+    "x": 742,
+    "y": 366,
+    "group": "backend"
+  },
+  {
+    "ID": 319,
+    "NodeName": "Json",
+    "Description": "JavaScript Object Notation, lightweight data-interchange format easy for humans to read and write.",
+    "x": 741,
+    "y": 148,
+    "group": "backend"
+  },
+  {
+    "ID": 320,
+    "NodeName": "Auth",
+    "Description": "Authentication and authorization processes for securing applications and verifying user identities.",
+    "x": 980,
+    "y": 336,
+    "group": "backend"
+  },
+  {
+    "ID": 321,
+    "NodeName": "OAuth",
+    "Description": "Open standard for access delegation, commonly used for secure authorization in applications.",
+    "x": 935,
+    "y": 246,
+    "group": "backend"
+  },
+  {
+    "ID": 322,
+    "NodeName": "Token",
+    "Description": "Digital authentication credential used for securing and maintaining user sessions.",
+    "x": 885,
+    "y": 140,
+    "group": "backend"
+  },
+  {
+    "ID": 323,
+    "NodeName": "Cookies",
+    "Description": "Small pieces of data stored on the client's computer used to remember stateful information.",
+    "x": 816,
+    "y": 217,
+    "group": "backend"
+  },
+  {
+    "ID": 324,
+    "NodeName": "Testing",
+    "Description": "Process of evaluating software to ensure it meets specified requirements and works as expected.",
+    "x": 688,
+    "y": 229,
+    "group": "backend"
+  },
+  {
+    "ID": 325,
+    "NodeName": "Integration",
+    "Description": "Phase in testing where individual software modules are combined and tested as a group.",
+    "x": 595,
+    "y": 338,
+    "group": "backend"
+  },
+  {
+    "ID": 326,
+    "NodeName": "Unit",
+    "Description": "Testing of individual components or units of source code to determine if they work correctly.",
+    "x": 831,
+    "y": 313,
+    "group": "backend"
+  },
+  {
+    "ID": 327,
+    "NodeName": "Functional",
+    "Description": "Testing that bases its test cases on the specifications of the software component under test.",
+    "x": 774,
+    "y": 425,
+    "group": "backend"
+  }
+    
+
+
+
+];
+
+export default nodes;
--- app/data/FrontEndNodes.js
+++ app/data/FrontEndNodes.js
@@ -10,249 +10,191 @@ const data = [
     "ID": 1,
     "NodeName": "HTML",
     "Description": "Covers various aspects of mastering HTML for building web pages.",
-    "from": -1,
-    "x": 216,
-    "y": -151
+    "x": 214,
+    "y": -168
   },
   {
     "ID": 2,
     "NodeName": "HTML Basics",
     "Description": "Introduces the fundamental elements of HTML, including structure and syntax.",
-    "from": 1,
-    "x": 132,
-    "y": -162
+    "from": -1,
+    "x": 144,
+    "y": -91
   },
   {
     "ID": 3,
     "NodeName": "Semantic HTML",
     "Description": "Focuses on semantic tags like <header>, <footer>, and <article> to create meaningful web pages.",
     "from": 1,
-    "x": 165,
-    "y": -214
+    "x": 297,
+    "y": -165
   },
   {
     "ID": 4,
     "NodeName": "Forms and Validations",
     "Description": "Explores form creation and implementing client-side validation techniques.",
-    "from": 1,
-    "x": 209,
-    "y": -235
+    "from": 3,
+    "x": 260,
+    "y": -92
   },
   {
     "ID": 6,
     "NodeName": "Accessibility",
     "Description": "Covers techniques to make web pages accessible to all users, including proper semantic usage and ARIA roles.",
-    "from": 1,
-    "x": 122,
-    "y": -106
+    "from": 7,
+    "x": 124,
+    "y": -203
   },
   {
     "ID": 7,
     "NodeName": "Basics of SEO",
     "Description": "Introduces search engine optimization principles, including meta tags and text formatting.",
     "from": 1,
-    "x": 251,
-    "y": -228
+    "x": 193,
+    "y": -244
   },
   {
     "ID": 9,
     "NodeName": "CSS",
     "Description": "Focuses on styling and visual design for web applications.",
-    "from": 1,
-    "x": 348,
-    "y": -266
+    "x": 295,
+    "y": -250
   },
   {
     "ID": 10,
     "NodeName": "CSS Basics",
     "Description": "Covers basic CSS concepts, including selectors, properties, and the box model.",
-    "from": 9,
-    "x": 361,
-    "y": -339
+    "from": 15,
+    "x": 466,
+    "y": -272
   },
   {
     "ID": 11,
     "NodeName": "JavaScript",
     "Description": "Introduces dynamic and interactive web development using JavaScript.",
-    "from": 9,
-    "x": 461,
-    "y": -372
+    "from": "218",
+    "x": 804,
+    "y": -406
   },
   {
     "ID": 12,
     "NodeName": "JavaScript Basics",
     "Description": "Covers core JavaScript concepts such as variables, loops, and functions.",
     "from": 11,
-    "x": 427,
-    "y": -415
+    "x": 809,
+    "y": -533
   },
   {
     "ID": 13,
     "NodeName": "DOM Manipulation",
     "Description": "Focuses on using JavaScript to interact with and modify the DOM.",
     "from": 11,
-    "x": 475,
-    "y": -425
+    "x": 922,
+    "y": -366
   },
   {
     "ID": 14,
     "NodeName": "Fetch API",
     "Description": "Explains how to use the Fetch API for making HTTP requests and handling responses.",
     "from": 11,
-    "x": 417,
-    "y": -368
+    "x": 903,
+    "y": -496
   },
   {
     "ID": 15,
     "NodeName": "Making Layouts",
     "Description": "Explores CSS layout techniques like Flexbox and Grid for responsive design.",
-    "from": 9,
-    "x": 283,
-    "y": -281
+    "from": 16,
+    "x": 389,
+    "y": -362
   },
   {
     "ID": 16,
     "NodeName": "Responsive Web Design",
     "Description": "Covers techniques for creating web pages that adapt to different screen sizes.",
     "from": 9,
-    "x": 316,
-    "y": -318
+    "x": 300,
+    "y": -341
   },
   {
     "ID": 17,
     "NodeName": "React",
     "Description": "Explains how to use the Fetch API for making HTTP requests and handling responses.",
-    "from": 11,
-    "x": 588,
-    "y": -478
+    "from": "218",
+    "x": 593,
+    "y": -470
   },
   {
     "ID": 18,
     "NodeName": "React Basics",
     "Description": "Introduces React, a JavaScript library for building user interfaces.",
     "from": 17,
-    "x": 576,
-    "y": -540
+    "x": 529,
+    "y": -570
   },
   {
     "ID": 19,
     "NodeName": "React Components",
     "Description": "Covers React components, props, and state for building reusable UI elements.",
     "from": 17,
-    "x": 524,
-    "y": -526
+    "x": 483,
+    "y": -503
   },
   {
     "ID": 20,
     "NodeName": "React Hooks",
     "Description": "Explores React Hooks for managing state and side effects in functional components.",
     "from": 17,
-    "x": 622,
-    "y": -539
+    "x": 604,
+    "y": -581
   },
   {
     "ID": 21,
     "NodeName": "React Router",
     "Description": "Introduces React Router for adding navigation and routing to React applications.",
     "from": 17,
-    "x": 669,
-    "y": -475
+    "x": 690,
+    "y": -498
   },
   {
     "ID": 22,
     "NodeName": "Redux",
     "Description": "Covers Redux, a predictable state container for managing application state.",
     "from": 17,
-    "x": 660,
-    "y": -525
-  },
-  {
-    "ID": 101,
-    "NodeName": "Python",
-    "Description": "Introduces Python programming with a focus on backend development.",
-    "from": -1,
-    "x": -105,
-    "y": 21
-  },
-  {
-    "ID": 102,
-    "NodeName": "Django",
-    "Description": "Covers Django, a Python framework for building scalable and robust web applications.",
-    "from": 101,
-    "x": -331,
-    "y": 35
-  },
-  {
-    "ID": 103,
-    "NodeName": "Django Rest Framework",
-    "Description": "Explores building RESTful APIs using Django Rest Framework.",
-    "from": 102,
-    "x": -503,
-    "y": 53
-  },
-  {
-    "ID": 104,
-    "NodeName": "Flask",
-    "Description": "Introduces Flask, a lightweight Python framework for building web applications.",
-    "from": 101,
-    "x": -305,
-    "y": -41
-  },
-  {
-    "ID": 105,
-    "NodeName": "Java",
-    "Description": "Introduces Java programming for backend systems.",
-    "from": -1,
-    "x": 176,
-    "y": 188
-  },
-  {
-    "ID": 106,
-    "NodeName": "Spring Boot",
-    "Description": "Covers Spring Boot, a Java framework for building microservices and enterprise applications.",
-    "from": 105,
-    "x": 230,
-    "y": 157
-  },
-  {
-    "ID": 107,
-    "NodeName": "JavaFX",
-    "Description": "Explores JavaFX for building rich client applications with Java.",
-    "from": 105,
-    "x": 220,
-    "y": 241
+    "x": 668,
+    "y": -570
   },
   {
     "ID": 108,
     "NodeName": "Relational Databases",
     "Description": "Covers relational database concepts and SQL for data management.",
     "from": -1,
-    "x": 219,
-    "y": 18
+    "x": 111,
+    "y": 192
   },
   {
     "ID": 109,
     "NodeName": "SQL",
     "Description": "Introduces SQL for querying and managing relational databases.",
     "from": 108,
-    "x": 400,
-    "y": -1
+    "x": 94,
+    "y": 368
   },
   {
     "ID": 110,
     "NodeName": "MySQL",
     "Description": "Covers MySQL, an open-source relational database management system.",
     "from": 109,
-    "x": 461,
-    "y": -43
+    "x": 203,
+    "y": 401
   },
   {
     "ID": 111,
     "NodeName": "PostgreSQL",
     "Description": "Introduces PostgreSQL, an open-source object-relational database system.",
     "from": 109,
-    "x": 540,
-    "y": -343
+    "x": 264,
+    "y": 239
   },
   {
     "ID": 201,
@@ -271,100 +213,60 @@ const data = [
     "y": -495
   },
   {
-    "ID": 203,
-    "NodeName": "Git",
-    "Description": "Introduces Git, a version control system for tracking changes in code.",
-    "from": -1,
-    "x": -143,
-    "y": -69
-  },
-  {
-    "ID": 204,
-    "NodeName": "GitHub",
-    "Description": "Covers using GitHub for collaboration and repository management.",
-    "from": 203,
-    "x": -302,
-    "y": -224
-  },
-  {
-    "ID": 205,
-    "NodeName": "CI/CD",
-    "Description": "Explores Continuous Integration and Continuous Deployment pipelines.",
-    "from": 204,
-    "x": -451,
-    "y": -359
-  },
-  {
-    "ID": 206,
-    "NodeName": "Jenkins",
-    "Description": "Covers using Jenkins for automating CI/CD workflows.",
-    "from": 205,
-    "x": -457,
-    "y": -528
-  },
-  {
-    "ID": 207,
-    "NodeName": "Travis CI",
-    "Description": "Introduces Travis CI for automating build and deployment processes.",
-    "from": 205,
-    "x": -598,
-    "y": -383
-  },
-  {
     "ID": 208,
     "NodeName": "AWS",
     "Description": "Covers Amazon Web Services for cloud computing solutions.",
     "from": -1,
-    "x": 99,
-    "y": -412
+    "x": 1,
+    "y": -439
   },
   {
     "ID": 209,
     "NodeName": "EC2",
     "Description": "Explores Amazon EC2 for scalable cloud computing instances.",
     "from": 208,
-    "x": 145,
-    "y": -321
+    "x": 93,
+    "y": -399
   },
   {
     "ID": 210,
     "NodeName": "S3",
     "Description": "Covers Amazon S3 for object storage solutions.",
     "from": 208,
-    "x": 392,
-    "y": -480
+    "x": 102,
+    "y": -471
   },
   {
     "ID": 211,
     "NodeName": "RDS",
     "Description": "Introduces Amazon RDS for managed relational databases.",
     "from": 208,
-    "x": 310,
-    "y": -611
+    "x": 132,
+    "y": -569
   },
   {
     "ID": 212,
     "NodeName": "Lambda",
     "Description": "Explores AWS Lambda for serverless computing.",
     "from": 208,
-    "x": 165,
-    "y": -521
+    "x": 40,
+    "y": -554
   },
   {
     "ID": 214,
     "NodeName": "CloudFront",
     "Description": "Covers AWS CloudFront for content delivery and caching.",
     "from": 208,
-    "x": 222,
-    "y": -567
+    "x": -80,
+    "y": -549
   },
   {
     "ID": 215,
     "NodeName": "Route 53",
     "Description": "Explores AWS Route 53 for DNS and domain management.",
     "from": 208,
-    "x": 71,
-    "y": -582
+    "x": -10,
+    "y": -618
   },
   {
     "ID": 216,
@@ -381,6 +283,20 @@ const data = [
     "from": 202,
     "x": -308,
     "y": -628
+  },
+  {
+    "ID": "218",
+    "NodeName": false,
+    "Description": "Something",
+    "from": 10,
+    "x": 607,
+    "y": -367
+  },
+  {
+    "ID": "219",
+    "NodeName": false,
+    "x": 786,
+    "y": -48
   }
 
 ];
