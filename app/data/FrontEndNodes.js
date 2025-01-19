2f0305b9e586f125215a9d8b5aa25a6d10d7ba27
Diaggio <123846531+Diaggio@users.noreply.github.com>
2025-01-19 09:44:30 -0800

    fixed position of nodes

--- app/data/FrontEndNodes.js
+++ app/data/FrontEndNodes.js
@@ -4,95 +4,87 @@ const data = [
     "ID": -1,
     "NodeName": "User",
     "Description": "Represents a user's skill tree.",
-    "x": 34,
-    "y": 6,
+    "x": 331,
+    "y": 107,
     "group": "frontend"
   },
   {
     "ID": 1,
     "NodeName": "HTML",
     "Description": "Covers various aspects of mastering HTML for building web pages.",
-    "x": 214,
-    "y": -168,
+    "x": 198,
+    "y": -212,
     "group": "frontend"
   },
   {
     "ID": 2,
     "NodeName": "HTML Basics",
     "Description": "Introduces the fundamental elements of HTML, including structure and syntax.",
-    "from": -1,
-    "x": 144,
-    "y": -91,
+    "x": 282,
+    "y": -20,
     "group": "frontend"
   },
   {
     "ID": 3,
     "NodeName": "Semantic HTML",
     "Description": "Focuses on semantic tags like &lt;header&gt;, &lt;footer&gt;, and &lt;article&gt; to create meaningful web pages.",
-    "from": 1,
-    "x": 297,
-    "y": -165,
+    "x": 255,
+    "y": -108,
     "group": "frontend"
   },
   {
     "ID": 4,
     "NodeName": "Forms and Validations",
     "Description": "Explores form creation and implementing client-side validation techniques.",
-    "from": 3,
-    "x": 260,
-    "y": -92,
+    "x": 330,
+    "y": -101,
     "group": "frontend"
   },
   {
     "ID": 6,
     "NodeName": "Accessibility",
     "Description": "Covers techniques to make web pages accessible to all users, including proper semantic usage and ARIA roles.",
-    "from": 7,
-    "x": 124,
-    "y": -203,
+    "x": 135,
+    "y": -30,
     "group": "frontend"
   },
   {
     "ID": 7,
     "NodeName": "Basics of SEO",
     "Description": "Introduces search engine optimization principles, including meta tags and text formatting.",
-    "from": 1,
-    "x": 193,
-    "y": -244,
+    "x": 133,
+    "y": -135,
     "group": "frontend"
   },
   {
     "ID": 9,
     "NodeName": "CSS",
     "Description": "Focuses on styling and visual design for web applications.",
-    "x": 295,
-    "y": -250,
+    "x": 71,
+    "y": -286,
     "group": "frontend"
   },
   {
     "ID": 10,
     "NodeName": "CSS Basics",
     "Description": "Covers basic CSS concepts, including selectors, properties, and the box model.",
-    "from": 15,
-    "x": 466,
-    "y": -272,
+    "x": 247,
+    "y": -318,
     "group": "frontend"
   },
   {
     "ID": 11,
     "NodeName": "JavaScript",
     "Description": "Introduces dynamic and interactive web development using JavaScript.",
-    "from": "218",
-    "x": 804,
-    "y": -406,
+    "x": 560,
+    "y": -378,
     "group": "frontend"
   },
   {
     "ID": 12,
     "NodeName": "JavaScript Basics",
     "Description": "Covers core JavaScript concepts such as variables, loops, and functions.",
-    "from": 11,
-    "x": 809,
+    "x": 582,
     "y": -533,
     "group": "frontend"
   },
@@ -100,200 +92,90 @@ const data = [
     "ID": 13,
     "NodeName": "DOM Manipulation",
     "Description": "Focuses on using JavaScript to interact with and modify the DOM.",
-    "from": 11,
-    "x": 922,
-    "y": -366,
+    "x": 692,
+    "y": -413,
     "group": "frontend"
   },
   {
     "ID": 14,
     "NodeName": "Fetch API",
     "Description": "Explains how to use the Fetch API for making HTTP requests and handling responses.",
-    "from": 11,
-    "x": 903,
-    "y": -496,
+    "x": 704,
+    "y": -518,
     "group": "frontend"
   },
   {
     "ID": 15,
     "NodeName": "Making Layouts",
     "Description": "Explores CSS layout techniques like Flexbox and Grid for responsive design.",
-    "from": 16,
-    "x": 389,
-    "y": -362,
+    "x": 198,
+    "y": -409,
     "group": "frontend"
   },
   {
     "ID": 16,
     "NodeName": "Responsive Web Design",
     "Description": "Covers techniques for creating web pages that adapt to different screen sizes.",
-    "from": 9,
-    "x": 300,
-    "y": -341,
+    "x": 105,
+    "y": -390,
     "group": "frontend"
   },
   {
     "ID": 17,
     "NodeName": "React",
     "Description": "Explains how to use the Fetch API for making HTTP requests and handling responses.",
-    "from": "218",
-    "x": 593,
-    "y": -470,
+    "x": 328,
+    "y": -431,
     "group": "frontend"
   },
   {
     "ID": 18,
     "NodeName": "React Basics",
     "Description": "Introduces React, a JavaScript library for building user interfaces.",
-    "from": 17,
-    "x": 529,
-    "y": -570,
+    "x": 286,
+    "y": -563,
     "group": "frontend"
   },
   {
     "ID": 19,
     "NodeName": "React Components",
     "Description": "Covers React components, props, and state for building reusable UI elements.",
-    "from": 17,
-    "x": 483,
-    "y": -503,
+    "x": 231,
+    "y": -510,
     "group": "frontend"
   },
   {
     "ID": 20,
     "NodeName": "React Hooks",
     "Description": "Explores React Hooks for managing state and side effects in functional components.",
-    "from": 17,
-    "x": 604,
-    "y": -581,
+    "x": 382,
+    "y": -588,
     "group": "frontend"
   },
   {
     "ID": 21,
     "NodeName": "React Router",
     "Description": "Introduces React Router for adding navigation and routing to React applications.",
-    "from": 17,
-    "x": 690,
-    "y": -498,
+    "x": 410,
+    "y": -452,
     "group": "frontend"
   },
   {
     "ID": 22,
     "NodeName": "Redux",
     "Description": "Covers Redux, a predictable state container for managing application state.",
-    "from": 17,
-    "x": 668,
-    "y": -570,
-    "group": "frontend"
-  },
-  {
-    "ID": 201,
-    "NodeName": "Docker",
-    "Description": "Introduces Docker for containerizing applications.",
-    "from": -1,
-    "x": -48,
-    "y": -127,
-    "group": "frontend"
-  },
-  {
-    "ID": 202,
-    "NodeName": "Kubernetes",
-    "Description": "Covers Kubernetes for container orchestration and deployment management.",
-    "from": 201,
-    "x": -270,
-    "y": -495,
-    "group": "frontend"
-  },
-  {
-    "ID": 208,
-    "NodeName": "AWS",
-    "Description": "Covers Amazon Web Services for cloud computing solutions.",
-    "from": -1,
-    "x": 1,
-    "y": -439,
-    "group": "frontend"
-  },
-  {
-    "ID": 209,
-    "NodeName": "EC2",
-    "Description": "Explores Amazon EC2 for scalable cloud computing instances.",
-    "from": 208,
-    "x": 93,
-    "y": -399,
-    "group": "frontend"
-  },
-  {
-    "ID": 210,
-    "NodeName": "S3",
-    "Description": "Covers Amazon S3 for object storage solutions.",
-    "from": 208,
-    "x": 102,
-    "y": -471,
-    "group": "frontend"
-  },
-  {
-    "ID": 211,
-    "NodeName": "RDS",
-    "Description": "Introduces Amazon RDS for managed relational databases.",
-    "from": 208,
-    "x": 132,
-    "y": -569,
-    "group": "frontend"
-  },
-  {
-    "ID": 212,
-    "NodeName": "Lambda",
-    "Description": "Explores AWS Lambda for serverless computing.",
-    "from": 208,
-    "x": 40,
-    "y": -554,
-    "group": "frontend"
-  },
-  {
-    "ID": 214,
-    "NodeName": "CloudFront",
-    "Description": "Covers AWS CloudFront for content delivery and caching.",
-    "from": 208,
-    "x": -80,
-    "y": -549,
-    "group": "frontend"
-  },
-  {
-    "ID": 215,
-    "NodeName": "Route 53",
-    "Description": "Explores AWS Route 53 for DNS and domain management.",
-    "from": 208,
-    "x": -10,
-    "y": -618,
-    "group": "frontend"
-  },
-  {
-    "ID": 216,
-    "NodeName": "Terraform",
-    "Description": "Introduces Terraform for infrastructure as code and automation.",
-    "from": 202,
-    "x": -136,
-    "y": -653,
-    "group": "frontend"
-  },
-  {
-    "ID": 217,
-    "NodeName": "Ansible",
-    "Description": "Introduces Ansible, an open-source automation tool for configuration management, application deployment, and orchestration.",
-    "from": 202,
-    "x": -308,
-    "y": -628,
+    "x": 440,
+    "y": -529,
     "group": "frontend"
   },
   {
     "ID": 218,
-    "NodeName": "FrontEndText",
-    "Description": "Something",
-    "from": 10,
-    "x": 607,
-    "y": -367,
+    "NodeName": "Front-End",
+    "Description": "",
+    "x": 422,
+    "y": -255,
     "group": "frontend"
-  },
+  }
 
 ];
 
